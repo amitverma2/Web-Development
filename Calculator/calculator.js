3cd811c5330dee7f5c8e0f04bcd1405a7cd92dfd
Amit Verma <amit.verma2@gmail.com>
2022-08-03 03:22:43 +0530

    basic calculator server side

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  res.send("Hello, world");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body);
+  var result = Number(req.body.num1) + Number(req.body.num2);
+  res.send("The result is " + result);
 });
 
 app.listen(3000, function() {
-  console.log("Started calculator sever at port 3000");
+  console.log("Started calculator sever at port 3000 and directory " + __dirname);
 });
